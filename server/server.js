863a520ddcbb6baba9dcd24266f75a8f7c7d4322
adampodkdev@gmail.com <adampodkdev@gmail.com>
2021-09-25 15:17:43 +0200

    add express

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
-console.log("siema");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://127.0.0.1:${PORT}`);
+});
