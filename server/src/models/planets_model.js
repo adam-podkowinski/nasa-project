a977b10c721b7f364d07512031969ff384637f5d
adampodkdev@gmail.com <adampodkdev@gmail.com>
2021-09-25 18:27:53 +0200

    Add gitignores and package.json refactor

---
+++ server/src/models/planets_model.js
@@ -0,0 +1,38 @@
+const parse = require("csv-parse");
+const fs = require("fs");
+const path = require("path");
+
+const habitablePlanets = [];
+
+const isHabitablePlanet = (planet) =>
+  planet["koi_disposition"] === "CONFIRMED" &&
+  planet["koi_insol"] > 0.36 &&
+  planet["koi_insol"] < 1.11 &&
+  planet["koi_prad"] < 1.6;
+
+const loadPlanetsData = () => {
+  return new Promise((resolve, reject) => {
+    const readStream = fs.createReadStream(
+      path.join(__dirname, "..", "..", "data", "kepler_data.csv")
+    );
+
+    readStream
+      .pipe(parse({ comment: "#", columns: true }))
+      .on("data", (data) => {
+        if (isHabitablePlanet(data)) habitablePlanets.push(data);
+      })
+      .on("error", (error) => {
+        console.log(error);
+        reject(error);
+      })
+      .on("end", () => {
+        console.log(`${habitablePlanets.length} habitable planets found`);
+        resolve();
+      });
+  });
+};
+
+module.exports = {
+  loadPlanetsData,
+  planets: habitablePlanets,
+};
---
+++ server/src/server.js
@@ -0,0 +1,22 @@
+const http = require("http");
+
+const app = require("./app");
+
+const { loadPlanetsData } = require("./models/planets_model");
+
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+const startServer = async () => {
+  await loadPlanetsData();
+
+  server.listen(PORT, () => {
+    console.log(`Listening on http://127.0.0.1:${PORT}`);
+    console.log("siema");
+  });
+};
+
+startServer().catch((e) => {
+  console.log(e);
+});
